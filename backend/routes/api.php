6c70fb301fd1758bb57056ee4a2eb50ea85a8c00
John Kristan Torremocha <johnkristan01@gmail.com>
2024-08-23 08:19:15 +0800

    setup the different can access router for each role

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,12 +1,23 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\PropertyController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    
+    Route::middleware('role:client')->group(function () {
+        Route::get('/client', [PropertyController::class, 'client']);
+    });
+
+    Route::middleware('role:client_representative')->group(function () {
+        Route::get('/client_representative', [PropertyController::class, 'client_representative']);
+    });
+    
+});
+
+
