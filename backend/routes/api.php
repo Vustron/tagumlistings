04363562123b3912145147e381b0deda80927f7c
John Kristan Torremocha <johnkristan01@gmail.com>
2024-08-24 00:11:45 +0800

    created the endpoint of update and logout

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,6 +17,9 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::middleware('role:client_representative')->group(function () {
         Route::get('/client_representative', [PropertyController::class, 'client_representative']);
     });
+
+    Route::put('/user/update', [AuthController::class, 'update']);
+    Route::post('/logout', [AuthController::class, 'logout']);
     
 });
 
