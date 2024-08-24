bddc521a00efcf8b3d942c5e6c6580f4425fbe41
John Kristan Torremocha <johnkristan01@gmail.com>
2024-08-24 15:59:21 +0800

    setup the routes for reserve and sold property

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,20 +4,26 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PropertyController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('guest')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+});
 
 
-Route::middleware('auth:sanctum')->group(function () {
+
+Route::middleware(['auth:sanctum', 'check.token.expiration'])->group(function () {
     
     Route::middleware('role:client')->group(function () {
         Route::get('/client', [PropertyController::class, 'client']);
     });
 
     Route::middleware('role:client_representative')->group(function () {
-        Route::get('/client_representative', [PropertyController::class, 'client_representative']);
+        Route::apiResource('property', PropertyController::class);
+        
+        Route::put('/reserve/property/{property}', [PropertyController::class, 'reserve']);
+        Route::put('/sold/property/{property}', [PropertyController::class, 'sold']);
     });
-
+    
     Route::put('/user/update', [AuthController::class, 'update']);
     Route::post('/logout', [AuthController::class, 'logout']);
     
