f56fd3f954d490b1b86f48d992f8228b89dd78fc
John Kristan Torremocha <johnkristan01@gmail.com>
2024-08-24 15:56:32 +0800

    created a token expiration checker middleware

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\CheckTokenExpiration;
 use App\Http\Middleware\RoleMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -14,7 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
-            'role' => RoleMiddleware::class
+            'role' => RoleMiddleware::class,
+            'check.token.expiration' => CheckTokenExpiration::class
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
