28faea096f37e63dd1c6456aecb9382125bfa750
John Kristan Torremocha <johnkristan01@gmail.com>
2024-08-27 06:02:59 +0800

    chores: added new column in properties migration name, description and price

--- backend/database/migrations/2024_08_24_020017_create_properties_table.php
+++ backend/database/migrations/2024_08_24_020017_create_properties_table.php
@@ -14,9 +14,13 @@ return new class extends Migration
         Schema::create('properties', function (Blueprint $table) {
             $table->id();
             $table->json('propertyPics')->nullable();
+            $table->string('property_name');
+            $table->string('description');
             $table->string('category');
             $table->string('location');
             $table->string('status')->default('available');
+            $table->decimal('price', 10, 2);
+            $table->decimal('reservation_fee', 10, 2);
             $table->timestamps();
             $table->foreignId('user_id')->nullable();
         });
