eb48d3a5adfac04adc56f185ff99d4b21992641e
John Kristan Torremocha <johnkristan01@gmail.com>
2024-08-27 16:20:04 +0800

    feat  added new user fields in migration columns

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('address');
+            $table->string('name')->nullable();
+            $table->string('address')->nullable();
             $table->string('contact_number', 20)->nullable();
             $table->string('email')->unique();
             $table->string('password');
