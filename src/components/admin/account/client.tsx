9b612ee4d5312cc7ec72d464cf9cf0fd6af799e4
Vustron Vustronus <runa.michaeljoshua@dnsc.edu.ph>
2024-10-01 19:39:28 +0800

    feat: refractored the loading state on account form

--- src/components/admin/account/client.tsx
+++ src/components/admin/account/client.tsx
@@ -1,28 +1,29 @@
 "use client"
 
 // components
+import { ErrorFallback, LoadingFallback } from "@/components/shared/fallback"
 import AccountForm from "@/components/admin/account/form"
 import { Separator } from "@/components/ui/separator"
 import { Heading } from "@/components/ui/heading"
-import { Loader2 } from "lucide-react"
 
 // hooks
 import { useGetAccount } from "@/lib/hooks/auth/get"
-// import { useRouter } from "next/navigation"
 
 // utils
+import { ErrorBoundary } from "react-error-boundary"
 import { isValidSessionData } from "@/lib/utils"
 import { dataSerializer } from "@/lib/utils"
+import { Suspense } from "react"
 
 // types
-import type { SessionData } from "@/lib/config/session"
+import type { UserData } from "@/lib/types"
 
 const AccountClient = ({ id }: { id?: string }) => {
-  const { data: user, isLoading, error, status } = useGetAccount(id!)
+  const { data: user } = useGetAccount(id!)
 
-  const userData: SessionData | undefined =
+  const userData: UserData | undefined =
     user && isValidSessionData(user)
-      ? dataSerializer<SessionData>(user)
+      ? dataSerializer<UserData>(user)
       : undefined
 
   return (
@@ -36,16 +37,11 @@ const AccountClient = ({ id }: { id?: string }) => {
       <Separator className="mt-2" />
 
       <div className="container flex flex-col justify-center items-center lg:w-[400px] sm:w-[300px] h-auto p-5 mt-5">
-        {isLoading ? (
-          <Loader2 className="animate-spin size-6" />
-        ) : status === "error" ? (
-          <span className="absolute inset-0 flex items-center justify-center">
-            {error?.message}
-          </span>
-        ) : (
-          status === "success" &&
-          userData && <AccountForm id={id} data={userData} />
-        )}
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+          <Suspense fallback={<LoadingFallback />}>
+            <AccountForm id={id} data={userData!} />
+          </Suspense>
+        </ErrorBoundary>
       </div>
     </>
   )
